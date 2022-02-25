1176a7697fc77dc2d2d8532b9e675c6397d380d1
ali-ahmed-05 <aliahmed.pluton@gmail.com>
2022-02-25 16:05:12 +0500

    up

--- package/addresses/localhost.js
+++ package/addresses/localhost.js
@@ -1,10 +1,10 @@
 
-   const accountDeployer = "0xE8F7d98bE6722d42F29b50500B0E318EF2be4fc8"
-   const badge = "0xe38b6847E611e942E6c80eD89aE867F522402e80"
-   const wallet = "0x2c8ED11fd7A058096F2e5828799c68BE88744E2F"
-   const nft = "0x7580708993de7CA120E957A62f26A5dDD4b3D8aC"
-   const rentVault = "0x75c68e69775fA3E9DD38eA32E554f6BF259C1135"
-   const eRC2099starz = "0xb932C8342106776E73E39D695F3FFC3A9624eCE0"
+   const accountDeployer = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+   const badge = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+   const wallet = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+   const nft = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
+   const rentVault = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
+   const eRC2099starz = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
    module.exports = {accountDeployer,badge,wallet,nft,rentVault,eRC2099starz}
   
